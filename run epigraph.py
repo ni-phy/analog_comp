8f5e5750e954c7238fa51311bd8fe99351bcbeaf
Nikolas Hadjiantoni <nxh700@student.bham.ac.uk>
2024-06-17 19:47:49 +0100

    modiufied multiprocessing to work on cluster computing

--- run epigraph.py
+++ run epigraph.py
@@ -12,159 +12,139 @@ import nlopt
 import threading 
 from autograd import numpy as npa
 from mpi4py import MPI
+from mpi4py.futures import MPIPoolExecutor
 import queue
 
-def Singular_Gradient(trial, params, obj, perturb, ii, q):
+def Singular_Gradient(trial, params, obj, perturb, ii):
     N = len(trial)
     gradient = npa.zeros(N,dtype=np.double)
 
     positions = npa.copy(trial)
     positions[ii] +=   perturb
     objA = des.Objective(positions, params)
-    #positions[ii] -= 2*perturb
-    #objB = Objective(positions, params)
-    #gradient[ii] = (objA-objB)/(2.0*perturb)
     gradient[ii] = (objA-obj)/perturb
-    q.put(gradient)
     return gradient
 
-def par_grad(v, params, f0, perturb):
-    a = {}
-    gradients = npa.array(np.zeros(len(v)))
-
-    q = queue.Queue()
-    for i in range(len(v)):
-        a[i] = threading.Thread(target=Singular_Gradient, args=[v, params, f0, perturb, i, q])
-
-    for j in range(len(v)):
-        task = a[j]
-        task.start()
-        grad = q.get()
-        gradients[j] = grad[j]
-    
-    for k in range(len(v)):
-        task = a[k]
-        task.join()
-    
-    return gradients
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
+mpl.use('Agg')
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.sans-serif'] = ['Times New Roman']
+plt.rcParams['mathtext.fontset'] = 'cm'
+plt.rcParams['mathtext.rm'] = 'serif'
+
+#
+title = '../analog/trial/des_'
 if rank == 0:
-
-
-    mpl.use('Agg')
-    from mpl_toolkits.axes_grid1 import make_axes_locatable
-    plt.rcParams['font.family'] = 'serif'
-    plt.rcParams['font.sans-serif'] = ['Times New Roman']
-    plt.rcParams['mathtext.fontset'] = 'cm'
-    plt.rcParams['mathtext.rm'] = 'serif'
-
-    #
-    title = '../analog/trial/des_'
     print(title) #To keep track of slurm jobs
-    targetName = 'test_A.data'
-    posName = 'test_g_pos.data'
-    # parameters
-    vacuumC = 299792458
-    vacuumMu = 4.0e-7*np.pi
-    vacuumEpsilon = 1.0/vacuumMu/vacuumC/vacuumC
-    epHost = vacuumEpsilon
-    cHost = vacuumC
-    siEpsilon = 12*vacuumEpsilon
-    omega = 1e12
-    omegap = omega*2.1
-    omegac = np.sqrt( (omega/omegap)*(omega/omegap) - 1.0 + 0.25/(omega/omegap)/(omega/omegap) ) * omegap
-    wavelength = 2.0*np.pi*cHost/omega
-    radius = wavelength * 0.01
-    loss = 0.0
-    alphaG = gyro.GetAlpha_Gyro(omega,omegap,omegac,cHost,epHost,radius,loss)
-    alphaD = gyro.GetAlpha_Dielectric(omega,siEpsilon,cHost,epHost,radius,loss)
-    # design parameters
-    nAlpha = 20
-    alphas = []
-    atype = np.zeros(nAlpha,dtype=np.int32)
+targetName = 'test_A.data'
+posName = 'test_g_pos.data'
+# parameters
+vacuumC = 299792458
+vacuumMu = 4.0e-7*np.pi
+vacuumEpsilon = 1.0/vacuumMu/vacuumC/vacuumC
+epHost = vacuumEpsilon
+cHost = vacuumC
+siEpsilon = 12*vacuumEpsilon
+omega = 1e12
+omegap = omega*2.1
+omegac = np.sqrt( (omega/omegap)*(omega/omegap) - 1.0 + 0.25/(omega/omegap)/(omega/omegap) ) * omegap
+wavelength = 2.0*np.pi*cHost/omega
+radius = wavelength * 0.01
+loss = 0.0
+alphaG = gyro.GetAlpha_Gyro(omega,omegap,omegac,cHost,epHost,radius,loss)
+alphaD = gyro.GetAlpha_Dielectric(omega,siEpsilon,cHost,epHost,radius,loss)
+# design parameters
+nAlpha = 30
+alphas = []
+atype = np.zeros(nAlpha,dtype=np.int32)
 
-    ## atype == 0: dielectric; == 1: gyrotropic
+## atype == 0: dielectric; == 1: gyrotropic
 
-    #importing positions and adding random element
-    positions = np.zeros(nAlpha*2, dtype=np.double)
-    # with open(posName) as f:
-    #     data = [[float(num) for num in line.split()] for line in f]
-    # for jj in range(nAlpha):
-    #         positions[2*jj] = data[jj][0]+float(np.random.rand(1)-0.5)*10e-5
-    #         positions[2*jj+1] = data[jj][1]+float(np.random.rand(1)-0.5)*10e-5
-    #         atype[jj] = data[jj][2]
+#importing positions and adding random element
+positions = np.zeros(nAlpha*2, dtype=np.double)
+# with open(posName) as f:
+#     data = [[float(num) for num in line.split()] for line in f]
+# for jj in range(nAlpha):
+#         positions[2*jj] = data[jj][0]+float(np.random.rand(1)-0.5)*10e-5
+#         positions[2*jj+1] = data[jj][1]+float(np.random.rand(1)-0.5)*10e-5
+#         atype[jj] = data[jj][2]
 
-    #atype = np.random.randint(2,size=nAlpha)
-    atype = np.zeros(nAlpha)
-    # atype[:5] = 1
-    #atype = np.ones(nAlpha)
-    for ii in range(nAlpha):
-        if atype[ii] == 0:
-            alphas.append(alphaD)
-        else:
-            alphas.append(alphaG)
-    radii = np.ones(nAlpha)*radius
-    offset = 0.0
-    controlRadius = 6*wavelength
-    distFlag = 1
+#atype = np.random.randint(2,size=nAlpha)
+atype = np.zeros(nAlpha)
+# atype[:5] = 1
+#atype = np.ones(nAlpha)
+for ii in range(nAlpha):
+    if atype[ii] == 0:
+        alphas.append(alphaD)
+    else:
+        alphas.append(alphaG)
+radii = np.ones(nAlpha)*radius
+offset = 0.0
+controlRadius = 6*wavelength
+distFlag = 1
 
-    for ii in range(300):
-        rr = np.random.rand(nAlpha)*controlRadius
-        aa = np.random.rand(nAlpha)*2.0*np.pi
-        for jj in range(nAlpha):
-            positions[2*jj] = rr[jj]*np.cos(aa[jj])
-            positions[2*jj+1] = rr[jj]*np.sin(aa[jj])
-        if des.DistanceCheck(positions,radii,offset) == 0:
-            distFlag = 0
-            break
+for ii in range(300):
+    rr = np.random.rand(nAlpha)*controlRadius
+    aa = np.random.rand(nAlpha)*2.0*np.pi
+    for jj in range(nAlpha):
+        positions[2*jj] = rr[jj]*np.cos(aa[jj])
+        positions[2*jj+1] = rr[jj]*np.sin(aa[jj])
+    if des.DistanceCheck(positions,radii,offset) == 0:
+        distFlag = 0
+        break
 
-    distFlag = des.DistanceCheck(positions,radii,offset)
-    if distFlag == 1:
-        print("rerun")
-        exit()
-    # port definition
-    nPort = 5
-    obsRadius = controlRadius*1.5
-    normalize = True
-    #
-    with open(targetName) as f:
-        data = [[float(num) for num in line.split()] for line in f]
-    target = np.zeros((nPort,nPort),dtype=np.cdouble)
-    for ii in range(nPort):
-        for jj in range(nPort):
-            target[ii,jj] = data[ii][2*jj]+1j*data[ii][2*jj+1]
+distFlag = des.DistanceCheck(positions,radii,offset)
+if distFlag == 1:
+    print("rerun")
+    exit()
+# port definition
+nPort = 5
+obsRadius = controlRadius*1.5
+normalize = True
+#
+with open(targetName) as f:
+    data = [[float(num) for num in line.split()] for line in f]
+target = np.zeros((nPort,nPort),dtype=np.cdouble)
+for ii in range(nPort):
+    for jj in range(nPort):
+        target[ii,jj] = data[ii][2*jj]+1j*data[ii][2*jj+1]
 
-    params = [alphas,omega,cHost,epHost,nPort,obsRadius,normalize,target]
-    perturb = radius*1.0e-11
-    maxIter = 100
-    clength = radius*1.0e-1
+params = [alphas,omega,cHost,epHost,nPort,obsRadius,normalize,target]
+perturb = radius*1.0e-11
+maxIter = 100
+clength = radius*1.0e-1
 
-    eval_history = []
-    gl_hist = []
-    eval_it = [0]
-    max_f0 = [1e10]
-    best_position = positions
+eval_history = []
+gl_hist = []
+eval_it = [0]
+max_f0 = [1e10]
+best_position = positions
 
-    def global_obj(x, params, perturb):
-        f0 = des.Objective(x,params)
-        
-        print('iter: ', len(gl_hist), "Cost: ", f0, flush=True)
-        gl_hist.append(f0)
+def global_obj(x, params, perturb):
+    f0 = des.Objective(x,params)
+    
+    print('iter: ', len(gl_hist), "Cost: ", f0, flush=True)
+    gl_hist.append(f0)
 
-        if f0<max_f0[0]:
-            max_f0[0] = f0
-            global best_position
-            best_position = np.copy(x)
+    if f0<max_f0[0]:
+        max_f0[0] = f0
+        global best_position
+        best_position = np.copy(x)
 
-        return f0
+    return f0
 
-    f0 = des.Objective(positions, params)
+f0 = des.Objective(positions, params)
+def singular_gradient_wrapper(pos):
+    return Singular_Gradient(positions, params, f0, perturb, pos)
 
-    print('par_grad',par_grad(positions, params, f0, perturb))
 
+
+
+if rank==0:
     #Starting Global
     x = np.array(positions) 
 
@@ -176,7 +156,7 @@ if rank == 0:
     solver.set_lower_bounds(-controlRadius  *np.ones(len(positions)))
     solver.set_upper_bounds(controlRadius*np.ones(len(positions)))
     solver.set_min_objective(lambda a, g: global_obj(a, params, perturb))
-    solver.set_maxeval(50)
+    solver.set_maxeval(5000)
     solver.set_ftol_rel(1e-5)
     x[:] = solver.optimize(x)
     # i += 1
@@ -211,50 +191,50 @@ if rank == 0:
 
     #Starting Local
 
-    x = np.insert(best_position, 0, 1e2)
+x = np.insert(best_position, 0, 1e2)
 
-    algorithm = nlopt.LD_MMA
-    lb = -controlRadius*np.ones(len(best_position))
-    ub = controlRadius*np.ones(len(best_position))
+algorithm = nlopt.LD_MMA
+lb = -controlRadius*np.ones(len(best_position))
+ub = controlRadius*np.ones(len(best_position))
 
-    lb = np.insert(lb, 0, 0)
-    ub = np.insert(ub, 0, np.inf)
+lb = np.insert(lb, 0, 0)
+ub = np.insert(ub, 0, np.inf)
 
-    def f(x, grad):
-        t = x[0]  # "dummy" parameter
-        v = x[1:]  # design parameters
-        if grad.size > 0:
-            grad[0] = 1
-            grad[1:] = 0
-        return t
+def f(x, grad):
+    t = x[0]  # "dummy" parameter
+    v = x[1:]  # design parameters
+    if grad.size > 0:
+        grad[0] = 1
+        grad[1:] = 0
+    return t
+
+def c(result, x, gradient, params, perturb):
+    t = x[0]  # dummy parameter
+    v = x[1:] # design parameters
+
+    f0 = des.Objective(v, params)
+    if __name__ == '__main__':
+
+        with MPIPoolExecutor() as pool:
+            my_grad = npa.sum(list(pool.map(singular_gradient_wrapper, range(len(positions)))),axis=0)
+
+    print('Eval', eval_it, 'Cost', f0, flush=True)
+    eval_history.append(f0)
+    if f0<max_f0[0]:
+        max_f0[0] = f0
+        global best_position
+        best_position = np.copy(v)
+
+    # Assign gradients
+    if gradient.size > 0:
+        gradient[0,0] = -1  # gradient w.r.t. "t"
+        gradient[0,1:] = my_grad # gradient w.r.t. objective
+
+    result[:] = np.real(f0) - t
+    eval_it[-1] += 1
 
 
-
-    def c(result, x, gradient, params, perturb):
-        t = x[0]  # dummy parameter
-        v = x[1:] # design parameters
-
-        f0 = des.Objective(v, params)
-        my_grad = par_grad(v, params, f0, perturb) 
-
-        print('Eval', eval_it, 'Cost', f0, flush=True)
-        eval_history.append(f0)
-        if f0<max_f0[0]:
-            max_f0[0] = f0
-            global best_position
-            best_position = np.copy(v)
-
-        # Assign gradients
-        if gradient.size > 0:
-            gradient[0,0] = -1  # gradient w.r.t. "t"
-            gradient[0,1:] = my_grad # gradient w.r.t. objective
-
-        result[:] = np.real(f0) - t
-        eval_it[-1] += 1
-
-    run_num = 0
-
-
+if rank==0:
     for i in range(0,10):
         print('Optimization Number: ', i)
         x[1:] = best_position+np.random.randint(0,10, size=len(best_position))*controlRadius*10e-4
